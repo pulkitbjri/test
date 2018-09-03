9ac917b7db62b9e495dd11cc9457be5f61b220bf
pulkit nigam <pulkitbjri@gmail.com>
2018-09-03 20:20:02 +0530

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,12 +6,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @Configuration
 @EnableJpaRepositories
 @SpringBootApplication
 public class DemoApplication {
-
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
